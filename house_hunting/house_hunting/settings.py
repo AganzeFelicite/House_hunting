79cb02bd217d48f216810735dd06bd9280238010
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-18 12:09:39 +0200

    trying to render static

--- house_hunting/house_hunting/settings.py
+++ house_hunting/house_hunting/settings.py
@@ -293,7 +293,7 @@ STATIC_URL = '/static/'
 
 # Static files directories (for development)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 
