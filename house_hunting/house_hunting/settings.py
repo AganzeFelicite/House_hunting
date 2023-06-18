4fd449af0e0833d2058f1843d234d334eb9813aa
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-18 12:16:01 +0200

    trying to render static

--- house_hunting/house_hunting/settings.py
+++ house_hunting/house_hunting/settings.py
@@ -269,9 +269,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
