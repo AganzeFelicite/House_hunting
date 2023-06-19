ec62ff15935b4dbd1761551f8dc0e8960d6eb55f
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-19 06:43:55 +0200

    some changes

--- house_hunting/house_hunting/settings.py
+++ house_hunting/house_hunting/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -232,15 +233,16 @@ DATABASES = {
     }
 }
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/'
 
 # Specify the filesystem path where media files will be stored
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# if not os.path.exists(MEDIA_ROOT):
-#     os.makedirs(MEDIA_ROOT)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not os.path.exists(MEDIA_ROOT):
+    os.makedirs(MEDIA_ROOT)
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
