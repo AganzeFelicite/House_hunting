3688a3666e49e3a8808e32fe3158af0ed721416d
AganzeFelicite <aganzefelicite@gmail.com>
2023-06-19 06:01:04 +0200

    some changes

--- house_hunting/house_hunting/urls.py
+++ house_hunting/house_hunting/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('admins/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
